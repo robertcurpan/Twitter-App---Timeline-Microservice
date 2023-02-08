746af56985f347d5348bdb8a67c873c4b0e27cc4
Curpan Robert <robert-gabriel.curpan@student.tuiasi.ro>
2023-02-08 17:52:37 +0200

    First commit

---
+++ src/main/java/com/twitterapp/timelinemicroservice/TimelineMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.twitterapp.timelinemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelineMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelineMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twitterapp/timelinemicroservice/TimelineMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitterapp.timelinemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelineMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
