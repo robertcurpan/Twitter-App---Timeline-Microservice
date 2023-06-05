e4ab6400d9772882da6c8382630cd23afe46d881
Curpan Robert <robert-gabriel.curpan@student.tuiasi.ro>
2023-06-05 16:34:09 +0300

    Updated EC2 Ip

--- src/main/java/com/twitterapp/timelinemicroservice/controller/TimelineController.java
+++ src/main/java/com/twitterapp/timelinemicroservice/controller/TimelineController.java
@@ -24,7 +24,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/timeline")
-@CrossOrigin(value = {"http://34.245.12.59"})
+@CrossOrigin(value = {"http://54.228.142.58"})
 public class TimelineController {
 
     @Autowired
